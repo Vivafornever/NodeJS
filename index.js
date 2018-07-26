30cccb448eccf8323da6623b68e99bee3a4e63af
Vivafornever <stamenkovska.magdalena@gmail.com>
2018-07-26 21:33:08 +0200

    nodeJS - povtoruvanje.cas1

---
+++ index.js
@@ -0,0 +1,76 @@
+// console.log("hello from Node.js");
+
+// var integer1 = 5;
+// var integer2 = 10;
+
+// var sum = integer1+integer2;
+
+
+// console.log(integer1 + " + " + integer2 + " = " + sum);
+
+
+// var name = "Magdi";
+// var pass = "0000";
+// var nameUpdated = name.toLowerCase();
+
+// if(nameUpdated == "magdi"){
+// 	console.log("Hello " + name);
+
+// } else {
+// 	console.log("Fail");
+// }
+
+
+// switch(name) {
+// 	case "Magdi":
+// 	console.log("Hello " + name);
+// 	break;
+// 	case "MAGDI":
+// 	console.log("Hello " + name);
+// 	break;
+// 	default:
+// 	console.log("Fail");
+// }
+
+// console.log(name.toLowerCase());
+
+// if(name == "Magdi") {
+// 	console.log("hello" + name);
+// }else {
+// 	console.log ("Fail");
+// }
+
+// if(name == "Magdi" || name == "magdi" || name == "MAGDI"){
+// 	console.log("Hello " + name);
+// } else {
+// 	console.log("Fail");
+// }
+
+// var FirstName = "Magdi";
+// var LastName = "Ruzna";
+
+// var FullName = FirstName.concat(LastName);
+// console.log(FullName);
+
+// var boolean = true;
+// console.log(boolean);
+
+
+var integer = [ 2, 3, 4, 5, 6, 7, 8, 10, 12];
+
+integer[100] = 50;
+console.log(integer);
+
+// for (let i=0; i < integer.length; i++){
+//      console.log(integer[i] + ": ");
+
+// 	if(integer[i] % 3 == 0){
+// 		console.log("buzz");
+// 	} else if(integer[i] % 4 == 0){
+// 		console.log("fiz");
+// 	}
+
+// 	console.log( "\n");
+// }
+
+// bez else vo funkcija, ako odime smao so if - gi rpoveruva site uslovi za sekoj clen vo zadadena niza!!!
